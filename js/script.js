899fa40a4352dac98a20c8db17f2a2fa19644ded
Wulfazz <teito.klein@laposte.net>
2023-11-20 15:54:02 +0100

    project

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Sélection des persos 
+// cliquer sur la carte 
+    //Afichage d'îcones pour représenter les héros 
+    // Passer dessur ( hover ) fera grandir 
+        //montrer les caractéristiques
+        //Cliquer pour sélectionner le perso 
+        // joueur1 commence
+            // choix entre 1 à 5 pièces pour améliorer le perso
+            // L'écran du joueur1 est gris quand la sélection est faîte
+        //Au tour du joueur2
+        // Il faut que les 8 cartes soient choisies pour commencer
+        // Bouton commencer la partie apparaît
+    //Perso mort, filtre rouge = défaite
+    //Perso vivant, filtre bleu = victoire
+    // Si on peut on mettra le victory et defeat
+// La personne qui gagne 3 fois gagne la partie.
+// Prompt avec joueur 1 ou joueur 2
