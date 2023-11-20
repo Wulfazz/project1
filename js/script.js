eb67447cb805f4e2adc07dd41807e723684e2a5c
Wulfazz <teito.klein@laposte.net>
2023-11-20 16:17:43 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,17 +1,30 @@
+//Prompt avec les règles du jeu 
+    // si possible changement des noms de joueurs
+
 // Sélection des persos 
-// cliquer sur la carte 
-    //Afichage d'îcones pour représenter les héros 
-    // Passer dessur ( hover ) fera grandir 
-        //montrer les caractéristiques
-        //Cliquer pour sélectionner le perso 
+    // cliquer sur la carte 
+        //Afichage d'îcones pour représenter les héros 
+        // Passer dessur ( hover ) fera grandir 
+            //montrer les caractéristiques
+            //Cliquer pour sélectionner le perso 
         // joueur1 commence
-            // choix entre 1 à 5 pièces pour améliorer le perso
-            // L'écran du joueur1 est gris quand la sélection est faîte
+            
+                // L'écran du joueur1 est gris quand la sélection est faîte
         //Au tour du joueur2
-        // Il faut que les 8 cartes soient choisies pour commencer
-        // Bouton commencer la partie apparaît
-    //Perso mort, filtre rouge = défaite
-    //Perso vivant, filtre bleu = victoire
-    // Si on peut on mettra le victory et defeat
-// La personne qui gagne 3 fois gagne la partie.
-// Prompt avec joueur 1 ou joueur 2
+    // Il faut que les 8 cartes soient choisies pour commencer
+    // Bouton commencer la partie apparaît
+
+//Déroulé de la partie : 
+    //Choix des persos : Inclinaison de la carte sélectionnée 
+        //Le joueur choisi la carte qu'il souhaite jouer
+            // choix entre 1 à 5 pièces pour améliorer le perso
+            // 10 pièces maximum attribuable
+                // Défense totale : Pts de vie + bonus si pièces 
+                // Attaque totale : Pts attaque + bonus si pièces
+        //Si attaque < Défense  
+            //Perso mort, filtre rouge = défaite
+            //Perso vivant, filtre bleu = victoire
+            // Les cartes utilisées tournent à 90°
+    // La personne qui gagne 3 fois gagne la partie.
+        // Si on peut on mettra le victory et defeat à la fin de partie
+        // Prompt avec joueur 1 ou joueur 2
