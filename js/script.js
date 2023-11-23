1752d4a7becc5535c2d0dd2096f3c612506029fb
Wulfazz <teito.klein@laposte.net>
2023-11-23 13:51:15 +0100

    10deg

--- js/script.js
+++ js/script.js
@@ -28,3 +28,44 @@
     // La personne qui gagne 3 fois gagne la partie.
         // Si on peut on mettra le victory et defeat à la fin de partie
         // Prompt avec joueur 1 ou joueur 2
+
+// Constantes catégories
+
+    //deck1
+const fighter1 = document.querySelector('.fighter1');
+const mage1 = document.querySelector('.mage1');
+const marksman1 = document.querySelector('.marksman1');
+const support1 = document.querySelector('.support1');
+
+    //deck2
+const fighter2 = document.querySelector('.fighter2');
+const mage2 = document.querySelector('.mage2');
+const marksman2 = document.querySelector('.marksman2');
+const support2 = document.querySelector('.support2');
+
+//Permet de faire pencher la carte de 10° à droite
+function rotate10(combattants){
+    combattants.addEventListener('click', () => {
+        combattants.style.transform = "rotate(10deg)"
+    })
+}
+
+//deck1
+rotate10(fighter1)
+rotate10(mage1)
+rotate10(marksman1)
+rotate10(support1)
+
+//deck2
+rotate10(fighter2)
+rotate10(mage2)
+rotate10(marksman2)
+rotate10(support2)
+
+
+//compteurs +1 si 1 pas déclenché
+
+
+
+
+    
