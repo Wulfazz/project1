50e8dc54b07b743632b9002904de583137accd53
Wulfazz <teito.klein@laposte.net>
2023-11-24 11:01:13 +0100

    corec

--- js/script.js
+++ js/script.js
@@ -31,38 +31,33 @@
 
 // Constantes catégories
 
-    //deck1
+// Deck 1
 const fighter1 = document.querySelector('.fighter1');
 const mage1 = document.querySelector('.mage1');
 const marksman1 = document.querySelector('.marksman1');
 const support1 = document.querySelector('.support1');
 
-    //deck2
+// Deck 2
 const fighter2 = document.querySelector('.fighter2');
 const mage2 = document.querySelector('.mage2');
 const marksman2 = document.querySelector('.marksman2');
 const support2 = document.querySelector('.support2');
 
-//Permet de faire pencher la carte de 10° à droite
+// Permet de faire pencher la carte de 10° à droite
 let player1Counter = 0;
 let player2Counter = 0;
 
 // Ajout des valeurs d'attaque et de défense aux types de cartes
 const cardValues = {
-    fighter1: { attack: 3, defense: 2 },
-    mage1: { attack: 5, defense: 1 },
-    marksman1: { attack: 7, defense: 3 },
-    support1: { attack: 2, defense: 5 },
-
-    fighter2: { attack: 3, defense: 2 },
-    mage2: { attack: 5, defense: 1 },
-    marksman2: { attack: 7, defense: 3 },
-    support2: { attack: 2, defense: 5 }
+    fighter: { attack: 3, defense: 2 },
+    mage: { attack: 5, defense: 1 },
+    marksman: { attack: 7, defense: 3 },
+    support: { attack: 2, defense: 5 }
 };
 
 function rotate10(combattants, player, cardType) {
     combattants.addEventListener('click', () => {
-        if ((player === 1 && player1Counter < 1) || (player === 2 && player2Counter < 2)) {
+        if ((player === 1 && player1Counter < 1) || (player === 2 && player2Counter < 1)) {
             combattants.style.transform = "rotate(10deg)";
 
             if (player === 1) {
@@ -73,43 +68,64 @@ function rotate10(combattants, player, cardType) {
                 console.log("Joueur 2 - Compteur: " + player2Counter);
             }
 
-            compareCards();
-        }
-    });
-
-    // Fonction pour comparer les cartes après chaque rotation
-    function compareCards() {
-        if (player1Counter === 1 && player2Counter === 1) {
-            const player1Stats = cardValues[cardType + '1'];
-            const player2Stats = cardValues[cardType + '2'];
-
-            const player1Total = player1Stats.attack + player1Stats.defense;
-            const player2Total = player2Stats.attack + player2Stats.defense;
-
-            if (player1Total > player2Total) {
-                console.log("Joueur 1 gagne !");
-            } else if (player1Total < player2Total) {
-                console.log("Joueur 2 gagne !");
-            } else {
-                console.log("Match nul !");
+            if (player1Counter === 1 && player2Counter === 1) {
+                compareCards(cardType);
             }
         }
+    });
+}
+
+function compareCards(cardType) {
+    const player1Stats = cardValues[cardType];
+    const player2Stats = cardValues[cardType];
+
+    const player1Total = player1Stats.attack + player1Stats.defense;
+    const player2Total = player2Stats.attack + player2Stats.defense;
+
+    if (player1Total > player2Total) {
+        console.log("Joueur 1 gagne !");
+    } else if (player1Total < player2Total) {
+        console.log("Joueur 2 gagne !");
+    } else {
+        console.log("Match nul !");
+    }
+
+    // Sélectionner deux nouvelles cartes pour chaque joueur
+    selectNewCards(1);
+    selectNewCards(2);
+}
+
+function rotate90(combattants, player, cardType) {
+    combattants.style.transform = "rotate(90deg)";
+
+    if (player === 1) {
+        player1Counter++;
+        console.log("Joueur 1 - Compteur: " + player1Counter);
+    } else if (player === 2) {
+        player2Counter++;
+        console.log("Joueur 2 - Compteur: " + player2Counter);
+    }
+
+    if (player1Counter === 1 && player2Counter === 1) {
+        compareCards(cardType);
     }
 }
 
-// deck1
+// Fonction pour sélectionner de nouvelles cartes
+function selectNewCards(player) {
+    // Ajoutez ici la logique pour permettre au joueur de choisir deux nouvelles cartes
+    console.log("Joueur " + player + " choisit deux nouvelles cartes.");
+    // Vous pouvez utiliser une interface utilisateur, une boîte de dialogue, etc., pour permettre au joueur de choisir de nouvelles cartes.
+}
+
+// Deck 1
 rotate10(fighter1, 1, 'fighter');
 rotate10(mage1, 1, 'mage');
 rotate10(marksman1, 1, 'marksman');
 rotate10(support1, 1, 'support');
 
-// deck2
+// Deck 2
 rotate10(fighter2, 2, 'fighter');
 rotate10(mage2, 2, 'mage');
 rotate10(marksman2, 2, 'marksman');
 rotate10(support2, 2, 'support');
-
-
-
-
-    
